364ab7936af61d1b8e96b5415313362a9a32d5a5
Nick Goodall <nbgoodall@gmail.com>
2014-09-14 14:44:00 +0100

    Added Twitter search

--- twitter_mood.rb
+++ twitter_mood.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'data_mapper'
 require 'twitter'
+require 'sentimental'
 
 # Routes -----------------------------------------------------------------------
 
@@ -33,6 +34,7 @@ end
 get '/:name' do
 	@user = Score.first(:user => params[:name])
 	@mood = 'happy'
+	@tweets = tweets("nbgoodall")
 	#@score = Score.get params[:score]
 	@title = @user.user
 	erb :user
@@ -53,3 +55,24 @@ class Score
 end
 
 DataMapper.finalize.auto_upgrade!
+
+#Configure Twitter
+
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "BWZjGf2K44l9oMxl2fbpmIcIZ"
+  config.consumer_secret     = "o9xKwSzSbKwmpyaxDEQ5un58tQPEuzS30pKDmZfvZdkyu6lnXK"
+  config.access_token        = "469774692-95NWeVs7YMWGZNxYCyhNqqucoml3AwN6rVPsPDGB"
+  config.access_token_secret = "CJx9hZGLZeit4i8zY8GNp7vJWNA2NsTaLGEyTwcGkDwre"
+end
+
+def tweets(user)
+	$client.user_timeline(user)
+end
+
+#Sentiment Analysis
+
+Sentimental.load_defaults
+
+def score(tweet_array)
+	
+end
